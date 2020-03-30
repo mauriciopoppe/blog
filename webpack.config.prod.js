07e226381692b3620f1d23c0aeb8b8dd5d142a0f
Mauricio Poppe <Mauricio.Poppe@gmail.com>
2020-03-29 22:34:53 -0700

    change chunkFilenameExtension to js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
 
   output: {
     filename: '[name].[hash:5].js',
-    chunkFilename: '[id].[hash:5].css',
+    chunkFilename: '[id].[hash:5].js',
     library: '[name]'
   },
   devtool: 'cheap-source-map',
