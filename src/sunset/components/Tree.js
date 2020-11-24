003df5d57241cae052fdefbc41be0a7ff82ec1b9
Mauricio Poppe <Mauricio.Poppe@gmail.com>
2020-11-23 18:14:44 -0800

    attempt to use Tree code
    Signed-off-by: Mauricio Poppe <Mauricio.Poppe@gmail.com>

---
+++ src/sunset/components/Tree.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { animated, useSpring } from 'react-spring'
+import { symbol, symbolTriangle } from 'd3-shape'
+
+function genBody() {
+  return `
+      M -2.5 5
+      h 5
+      V 20
+      h -5
+      z`
+}
+
+function genTree() {
+  return `M   0  0
+        C   0 10  18 40   0 40
+        S          0 10   0  0
+        Z`
+}
+
+export const Tree = React.memo((props) => {
+  const { px, py } = props
+  // const [counter, setCounter] = useState(0)
+  // const [shootingStarProps, set] = useSpring(() => ({
+  //   cx: 0,
+  //   cy: 0
+  // }))
+  //
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     set({ cx: x(Math.random() * width), cy: y(Math.random() * height), r: 0 })
+  //     setCounter(counter + 1)
+  //   }, 3000)
+  // }, [set, width, height, counter])
+
+  return (
+    <g transform={`translate(${px} ${py})`}>
+      <animated.path {...props.pathStyle} stroke={'var(--primary)'} d={genBody()} />
+      <animated.path {...props.pathStyle} transform={`translate(0 -30)`} stroke={'var(--primary)'} d={genTree()} />
+    </g>
+  )
+})
