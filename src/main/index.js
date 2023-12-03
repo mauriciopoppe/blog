636c82451a83988962fade76246e51d12a03fa65
Mauricio Poppe <mauricio.poppe@gmail.com>
2023-12-02 23:02:17 -0500

    Disable pjax
    One problem of lazy loading content is with the libraries to import,
    it's possible that the first page visited doesn't import any of my
    libraries (function-plot, greuler, MathJax), if so then any other
    page that's loaded after this won't see these libraries included.
    
    I could include these libraries dynamically through JS code but
    now there's a mess of multiple places including libraries (hugo
    through includes or the scripts), I can try that in a take 2.

--- src/main/index.js
+++ src/main/index.js
@@ -1,39 +1,11 @@
-import { equationPreviewHotReload } from './equation-preview'
-import { footnotesPreviewHotReload } from './footnotes-preview'
-import { lazyLoadhotReload } from './lazy-load'
+import './equation-preview'
+import './footnotes-preview'
+import './lazy-load'
 import './sass/main.scss'
 import './sass/mobile.scss'
-import { heartHotReload } from './heart'
-import { sidebarHotReload } from './sidebar'
-import { sitemapHotReload } from './sitemap'
-import { initializeWithReloadFn } from './spa'
-
-initializeWithReloadFn({
-  onStart: function () {},
-  onComplete: function () {
-    lazyLoadhotReload()
-    sidebarHotReload()
-    sitemapHotReload()
-    heartHotReload()
-    equationPreviewHotReload()
-    footnotesPreviewHotReload()
-
-    // reload Mathjax if it's installed in the page
-    if ('MathJax' in window) {
-      // Reset equation numbering https://docs.mathjax.org/en/latest/web/typeset.html#resetting-automatic-equation-numbering
-      // to avoid `duplicated label` errors.
-      window.MathJax.texReset()
-      window.MathJax.typesetClear()
-      // Run MathJax asynchronously.
-      window.MathJax.typesetPromise()
-    }
-
-    // Set in site/layouts/partials/head.html
-    if ('myBlog' in window) {
-      window.myBlog.onLoad()
-    }
-  }
-})
+import './heart'
+import './sidebar'
+import './sitemap'
 
 export * from './font-face-observer'
 export * from './lazy-load'
